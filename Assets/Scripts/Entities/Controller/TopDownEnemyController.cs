7d378955578c70943aa1a35278d523787b6cd4ab
Zzondy-Unity <abdos230@naver.com>
2024-11-20 13:59:11 +0900

    [Chore]

--- Assets/Scripts/Entities/Controller/TopDownEnemyController.cs
+++ Assets/Scripts/Entities/Controller/TopDownEnemyController.cs
@@ -21,11 +21,13 @@ public class TopDownEnemyController : TopDownController
 
     protected float DistanceToTarget()
     {
+        if (ClosestTarget == null) return 0;
         return Vector3.Distance(transform.position, ClosestTarget.position);
     }
 
     protected Vector2 DirectionToTarget()
     {
+        if (ClosestTarget == null) return Vector2.zero;
         return (ClosestTarget.position - transform.position).normalized;
     }
 }
--- Assets/Scripts/Pickups/PickupStatModifier.cs
+++ Assets/Scripts/Pickups/PickupStatModifier.cs
@@ -18,7 +18,7 @@ public class PickupStatModifiers : PickupItem
         }
 
         //최대 체력을 올리거나 체력을 회복하는 경우
-        HealthSystem healthSystem = gameObject.GetComponent<HealthSystem>();
-        healthSystem.ChangeHealth(0);
+        //HealthSystem healthSystem = gameObject.GetComponent<HealthSystem>();
+        //healthSystem.ChangeHealth(0);
     }
 }
